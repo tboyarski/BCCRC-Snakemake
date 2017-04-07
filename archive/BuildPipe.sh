705192ea4a6e6c4cfc5e74e1302b24fd475e0ceb
tboyarski <tim.boyarski@gmail.com>
2017-04-07 14:06:44 -0700

    Updating Repo with Finalized pipeline vignette and Co-op presentation pptx from today

--- archive/BuildPipe.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-_SNAKEFILE="Snakefile"
-_YAMLFILE="NewYaml.yaml"
-python fileParse/fileParse.py line SampleSingleLines.txt NewYaml.yaml Snakefile
-python ReBam/ReBam.py NewYaml.yaml Snakefile
